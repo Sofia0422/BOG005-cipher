bd28df96226fb8e314413ebf08f25e8b987542e4
Sofia <81938344+Sofia0422@users.noreply.github.com>
2022-07-14 08:34:04 -0500

    Ingrese informacion a HTML, estilo a parrafos en CSS y logica en Js para ocultar texto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+//Ejecutar código de cifrado
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//Hacer visible sección para codificar mensajes y ocultar sección grupos principales
+function mostrarCodificarMensaje(){
+    document.getElementById("codificarMensaje").style.visibility = "visible";
+}
+
+function ocultarGruposPrincipales(){
+    document.getElementById("gruposPrincipales").style.visibility = "hidden";
+}
