26ae4a34a9e78b22dbf6dbb3213919df3134b22d
Sofia <81938344+Sofia0422@users.noreply.github.com>
2022-07-14 16:02:37 -0500

    codeo en JS para mostrar y ocultar elementos

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 //Hacer visible sección para codificar mensajes y ocultar sección grupos principales
-function mostrarCodificarMensaje(){
-    document.getElementById("codificarMensaje").style.visibility = "visible";
+/*function mostrarCodificarMensaje(){
+    document.getElementById('codificarMensaje').style.display = "block";
+}*/
+
+let botones = document.querySelectorAll("#botonGP")
+for (let i=0; i<botones.length; i++) {
+    botones[i].addEventListener("Click",function(e) {
+        let texto= this.nextElemtbyId
+    })
 }
 
-function ocultarGruposPrincipales(){
-    document.getElementById("gruposPrincipales").style.visibility = "hidden";
-}
