8f47cbffa8517842c967cdb00e46f450c8fb3318
Sofia <81938344+Sofia0422@users.noreply.github.com>
2022-07-15 16:45:45 -0500

    ajustes JS para ocultar y mostrar contenido sin exito

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 //Hacer visible sección para codificar mensajes y ocultar sección grupos principales
-/*function mostrarCodificarMensaje(){
-    document.getElementById('codificarMensaje').style.display = "block";
-}*/
 
-let botones = document.querySelectorAll("#botonGP")
-for (let i=0; i<botones.length; i++) {
-    botones[i].addEventListener("Click",function(e) {
-        let texto= this.nextElemtbyId
-    })
+function mostrarCodificarMensaje(){
+    document.getElementById("codificarMensaje").style.display = "block";
 }
 
+function ocultarCodificarMensaje(){
+    document.getElementById("codificarMensaje").style.display = "none";
+}
+
+
+/*let botonesGP = document.getElementById("botonesGP");
+let codificarMensaje = document.getElementById("codificarMensaje");
+
+botonesGP.addEventListener("click", toggleText);
+
+function toggleText(){
+    cofificarMensaje.classList.toggle("gruposPrincipales");
+
+    if(codificarMensaje.classList.contains("gruposPrincipales")) {
+        botonesGP.innerHTML = "inicio"
+    }
+    else{
+        botonesGP.innerHTML = "inicio"
+    }
+}*/
